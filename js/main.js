678bf04f2a06def79015b15e8d501b0609c05ff5
Monal Shadi <monalshadi16@gmail.com>
2017-07-06 01:08:27 +0530

    add js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function main () {
+  $('.skillset').hide();
+  $('.skillset').fadeIn(1000);
+  $('.projects').hide();
+  $('.projects-button').on('click',function(){
+    //$(this).next().toggle();
+    $(this).next().slideToggle(500);
+    $(this).toggleClass('active');
+    $(this).text('Projects Viewed');
+  } );
+}
+$(document).ready(main);
