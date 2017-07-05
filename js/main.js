023d89e96a653a1b0756a5dd7fbe53ef9dda0f75
Monal Shadi <monalshadi16@gmail.com>
2017-07-06 01:37:22 +0530

    minor js fix

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function main () {
   $('.projects').hide();
   $('.projects-button').on('click',function(){
     //$(this).next().toggle();
-    $(this).next().slideToggle(500);
+    $(this).next().slideToggle(400);
     $(this).toggleClass('active');
     $(this).text('Projects Viewed');
   } );
