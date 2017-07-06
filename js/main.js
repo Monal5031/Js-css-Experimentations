b68772123fbc9dfda910c5428b184ecc50d5b5f4
Monal Shadi <monalshadi16@gmail.com>
2017-07-06 06:01:54 +0530

    Added new commands on travis (#3)

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 function main () {
-  $('.skillset').hide();
-  $('.skillset').fadeIn(1000);
-  $('.projects').hide();
-  $('.projects-button').on('click',function(){
-    //$(this).next().toggle();
-    $(this).next().slideToggle(400);
-    $(this).toggleClass('active');
-    $(this).text('Projects Viewed');
-  } );
+	$('.skillset').hide();
+	$('.skillset').fadeIn(1000);
+	$('.projects').hide();
+	$('.projects-button').on('click', function () {
+		$(this).next().slideToggle(400);
+		$(this).toggleClass('active');
+		$(this).text('Projects Viewed');
+	});
 }
 $(document).ready(main);
