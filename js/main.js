1672031ef2dea5014c5dafb9483ca79e345f55d4
Melissa Thompson <mthompson828@gmail.com>
2017-10-02 08:54:41 -0400

    Move color picker to JS, add show/hide on click (#8)

--- js/main.js
+++ js/main.js
@@ -7,5 +7,17 @@ function main () {
 		$(this).toggleClass('active');
 		$(this).text('Projects Viewed');
 	});
+	$('#colorPickerLi').click( function() {
+		$('#colorPickerDisplay').toggleClass('hide');
+
+		if ($('#arrow').hasClass('glyphicon-chevron-down')) {
+			$('#arrow').removeClass('glyphicon-chevron-down');
+			$('#arrow').addClass('glyphicon-chevron-up');
+		} else {
+			$('#arrow').removeClass('glyphicon-chevron-up');
+			$('#arrow').addClass('glyphicon-chevron-down');
+		}
+		
+	});
 }
 $(document).ready(main);
