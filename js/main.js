ff597f94468c1898b76928f706028f7a0630bdae
amykw105 <amykw105@gmail.com>
2017-10-14 19:03:22 +0100

    track-listing in json

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ function main () {
 	$('.skillset').hide();
 	$('.skillset').fadeIn(1000);
 	$('.projects').hide();
+	$('.music').hide();
 	$('.projects-button').on('click', function () {
 		$(this).next().slideToggle(400);
 		$(this).toggleClass('active');
@@ -17,7 +18,40 @@ function main () {
 			$('#arrow').removeClass('glyphicon-chevron-up');
 			$('#arrow').addClass('glyphicon-chevron-down');
 		}
-		
+
 	});
-}
+
+	function getData(){
+		var div = $('.music');
+		$.ajax({
+		    type: 'GET',
+		    url: 'js/data.json',
+		    dataType: 'json',
+		    success: function (data) {
+					// Was this successful?
+
+					$.each(data.music, function(index, i) {
+						div.append('<div class="' + index + '"><h2>' + index + '</h2></div><div class="clearfix">');
+						var array = [i][0];
+						$(array).each(function (i, element){
+							var newClass = "." + index;
+							$(newClass).append('<div class="music-track"><img src="' + element.img + '"/><br/><a class="music-link" href="' + element.link + '" target="_blank">' + element.name + '</a><div>');
+						});
+
+	        });
+		    },
+				error: function (response) {
+					// Does this fail?
+					console.log(response);
+				}
+
+		});
+	}
+
+	getData();
+
+
+
+
+};
 $(document).ready(main);
